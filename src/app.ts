744a98a70079368d0904af52f3947451dae54480
Jakob Kosir <kosir.jakob@gmail.com>
2016-05-18 19:28:44 +0200

    clean.

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import {bootstrap} from "angular2/platform/browser";
-import {HTTP_PROVIDERS} from "angular2/http";
-import {SeedApp} from "./app/seed-app";
-
-
-bootstrap(SeedApp, [
-  HTTP_PROVIDERS
-])
-.catch(err => console.error(err));
